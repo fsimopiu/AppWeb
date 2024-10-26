c65f178824e843cb2388c845d4d9373fc62cf0f7
fsimopiu <franckwillysimopiugie@gmail.com>
2024-10-26 13:42:13 +0200

    ajout de la bibliothèque Axios

--- app-services/src/components/accueil/Accueil.vue
+++ app-services/src/components/accueil/Accueil.vue
@@ -9,6 +9,24 @@
         <!-- Sidebar -->
             <aside class="w-64 bg-gray-800 text-white p-4 h-screen">
                 <div class="relative mb-4">
+                    <div>
+                <b-dropdown class="services m-md-2" text="Mes Service">
+                    <template #button-content>
+                        <div style="display: inline;">
+                            <span><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg></span>
+                            <span class="dropdown-text">Mes services</span>
+                        </div>
+                    </template>
+                    <b-dropdown-item class="block px-4 py-2 hover:bg-gray-600">First Action</b-dropdown-item>
+                    <b-dropdown-item>Second Action</b-dropdown-item>
+                    <b-dropdown-item>Third Action</b-dropdown-item>
+                    <b-dropdown-divider></b-dropdown-divider>
+                    <b-dropdown-item active>Active action</b-dropdown-item>
+                    <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+                </b-dropdown>
+                </div>
                     <!-- Bouton pour afficher le menu déroulant -->
                     <button @click="toggleMenu" class="flex items-center">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
--- app-services/src/main.js
+++ app-services/src/main.js
@@ -1,8 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './style.css';
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+import * as BootstrapVueNext from 'bootstrap-vue-next';
+
+
+
+const app = createApp(App);
+app.use(router);
+
+// Enregistre tous les composants BootstrapVueNext globalement
+for (const [key, component] of Object.entries(BootstrapVueNext)) {
+    app.component(key, component);
+}
+
+app.mount('#app');
+
